fbca90b8c4b176ea142c539131e1b2322a239473
Ahsan mushtaq <ahsanmushtaq901@gmail.com>
2022-06-06 14:16:04 +0500

    code

---
+++ app.js
@@ -0,0 +1,13 @@
+
+alert("Find Greater of Two Numbers");
+
+var a, b;
+a = prompt("Enter first number");
+b = prompt("Enter second number");
+
+if(a>b){
+alert(a+"is greater " + b);
+}
+else{
+       alert(b + " is greater " + a);
+}
